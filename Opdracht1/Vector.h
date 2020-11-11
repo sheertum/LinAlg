ecaa69e292ac1e64b479cd2dd44961aa37d5fa30
sheertum <simonvanheertum@gmail.com>
2020-11-11 13:36:11 +0100

    Changed constructor

--- Opdracht1/Vector.h
+++ Opdracht1/Vector.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <vector>
+
 class Vector
 {
 public:
-    Vector(float coordinate, ...);
+    // Vector(float coordinate, ...);
+    Vector(std::vector<float>);
     Vector operator+(const Vector&);
     Vector operator-(const Vector&);
+    int dimensions;
+    std::vector<float> coordinates;
 private:
-    int _dimensions;
-    float _coordinate [];
-}
+    bool hasCorrectDimension(const Vector&);
+};
