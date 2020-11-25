46a4767794202c56a8c96b1c454e98eb3efa9150
sheertum <simonvanheertum@gmail.com>
2020-11-25 16:13:23 +0100

    Added an tested translation

--- LinAlg/LinAlg/Source/main.cpp
+++ LinAlg/LinAlg/Source/main.cpp
@@ -3,58 +3,86 @@
 #include <iostream>
 #include <SDL.h>
 #include "Matrix.h"
+#include "UnitaryMatrix.h"
 
 #undef main
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    Matrix left {3, 2};
-    Matrix right {2, 3};
+    Matrix matrix{ 6, 3 };
+    matrix(0, 0) = 1;
+    matrix(0, 1) = 1;
+    matrix(0, 2) = 1;
 
-    left(0, 0) = 1;
-    left(1, 0) = 2;
-    left(2, 0) = 3;
-    left(0, 1) = 4;
-    left(1, 1) = 5;
-    left(2, 1) = 6;
-
-    right(0, 0) = 7;
-    right(1, 0) = 8;
-    right(0, 1) = 9;
-    right(1, 1) = 10;
-    right(0, 2) = 11;
-    right(1, 2) = 12;
-
-    Matrix testMatrix(3,3);
-    testMatrix(0, 0) = 1;
-    testMatrix(1, 0) = 2;
-    testMatrix(2, 0) = 3;
-
-    testMatrix(0, 1) = 1;
-    testMatrix(1, 1) = 2;
-    testMatrix(2, 1) = 3;
-
-    testMatrix(0, 2) = 1;
-    testMatrix(1, 2) = 2;
-    testMatrix(2, 2) = 3;
-
-    Vector testVector{{ 1,2,3 } };
-
-    left.draw();
-
-    std::cout << std::endl;
-
-    right.draw();
-
-    std::cout << std::endl;
+    matrix(1, 0) = 4;
+    matrix(1, 1) = 4;
+    matrix(1, 2) = 4;
     
-    //Matrix result = left * right;
-    //result.draw();
+    matrix(2, 0) = 5;
+    matrix(2, 1) = 5;
+    matrix(2, 2) = 5;
 
-    std::cout << std::endl;
+    matrix(3, 0) = 6;
+    matrix(3, 1) = 6;
+    matrix(3, 2) = 6;
+
+    matrix(4, 0) = 7;
+    matrix(4, 1) = 7;
+    matrix(4, 2) = 7;
+
+    matrix(5, 0) = 8;
+    matrix(5, 1) = 8;
+    matrix(5, 2) = 8;
+
+    matrix.draw();
+    matrix.translate(1,1,1);
+    matrix.draw();
     
-    Matrix result = testMatrix * testVector;
-    result.draw();
+    // left(0, 0) = 1
+    ;
+    // left(1, 0) = 2;
+    // left(2, 0) = 3;
+    // left(0, 1) = 4;
+    // left(1, 1) = 5;
+    // left(2, 1) = 6;
+
+    // right(0, 0) = 7;
+    // right(1, 0) = 8;
+    // right(0, 1) = 9;
+    // right(1, 1) = 10;
+    // right(0, 2) = 11;
+    // right(1, 2) = 12;
+
+    // Matrix testMatrix(3,3);
+    // testMatrix(0, 0) = 1;
+    // testMatrix(1, 0) = 2;
+    // testMatrix(2, 0) = 3;
+
+    // testMatrix(0, 1) = 1;
+    // testMatrix(1, 1) = 2;
+    // testMatrix(2, 1) = 3;
+
+    // testMatrix(0, 2) = 1;
+    // testMatrix(1, 2) = 2;
+    // testMatrix(2, 2) = 3;
+
+    // Vector testVector{{ 1,2,3 } };
+
+    // left.draw();
+
+    // std::cout << std::endl;
+
+    // right.draw();
+
+    // std::cout << std::endl;
+    
+    // //Matrix result = left * right;
+    // //result.draw();
+
+    // std::cout << std::endl;
+    
+    // Matrix result = testMatrix * testVector;
+    // result.draw();
     //test1.draw();
     //std::cout << std::endl;
     //test3.draw();
