ca2fe198e59217e50bb5f5496c2e543544420da1
sheertum <simonvanheertum@gmail.com>
2020-12-22 13:50:27 +0100

    Refactored figure to accept triangles, started on refactoring triangles

--- LinAlg/LinAlg/Headers/Triangle.h
+++ LinAlg/LinAlg/Headers/Triangle.h
@@ -7,6 +7,7 @@ class Triangle : public Matrix
 public:
 	Triangle(Vector, Vector, Vector);
 	Triangle(std::vector<double>);
+	std::vector<Vector> getVectors() const;
 private: 
 	std::vector<Vector> convertToVectors(Matrix);
 	BoundingBox _boundingBox;
