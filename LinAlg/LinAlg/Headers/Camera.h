58a78dfb70e7a09952bb4e4852443d6dc3653609
sheertum <simonvanheertum@gmail.com>
2020-12-30 11:25:00 +0100

    Set up first camera

--- LinAlg/LinAlg/Headers/Camera.h
+++ LinAlg/LinAlg/Headers/Camera.h
@@ -6,31 +6,23 @@
 
 class Camera {
 public:
-	Camera(const Vector& position, const Vector& lookAt, const Limit& xLimit, const Limit& yLimit, const Limit& zLimit);
-	Camera(const Vector& position, const Vector& lookAt, const Limit& limit);
-
-	void setXlimits(const Limit& limit);
-	void setYlimits(const Limit& limit);
-	void setZlimits(const Limit& limit);
-	void setLimits (const Limit& limit);
-
-	void update(const Vector& position, const Vector& lookAt);
-	void update();
-
-	void rotate(double x, double y, double z);
-
-	std::vector<Vector> toCameraPerspective(const Vector& vector1, const Vector& vector2) const;
+	Camera(Vector position, Vector lookAt, double angle, double fov, Limit clippingPlane);
+	void roll(double angle);
+	void pitch(double angle);
+	void yaw(double angle);
+	void moveUp(double distance);
+	void moveRight(double distance);
+	void moveBack(double distance);
 
 private:
-	void updateTranslation(const Vector& right, const Vector& up, const Vector& direction);
+	Vector getXAxis();
+	Vector getYAxis();
+	Vector getZAxis();
 
 public:
-	Limit _xLimit;
-	Limit _yLimit;
-	Limit _zLimit;
-
+	double _fov;
 	Vector _position;
 	Vector _lookAt;
-
-	Matrix _translation;
+	Matrix _axis;
+	Limit _clippingPlane;
 };
